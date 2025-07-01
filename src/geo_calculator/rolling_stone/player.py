aba9b053beeb2c2764f1608bf576c5e7678f4050
Leo Lie McGrath <93575375+leomcgrath@users.noreply.github.com>
2025-07-01 08:36:43 +0000

    Added score for the player

--- src/geo_calculator/rolling_stone/player.py
+++ src/geo_calculator/rolling_stone/player.py
@@ -1,3 +1,6 @@
 class Player:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
+        self.score = 0
+
+    def receive_score(self, addScore):
+        self.score += addScore
